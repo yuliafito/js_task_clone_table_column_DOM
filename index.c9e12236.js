d40bfab39d7358195280dc73e57802b9dfd6f1ed
Yuliia Fito <jylia.fito2004@gmail.com>
2024-07-13 08:18:34 +0300

    make build

---
+++ index.c9e12236.js
@@ -0,0 +1,2 @@
+var e=document.querySelector("table").rows,r=!0,l=!1,t=void 0;try{for(var o,n=e[Symbol.iterator]();!(r=(o=n.next()).done);r=!0){var a=o.value,c=a.cells,i=c[1].cloneNode(!0),u=c.length-1;a.insertBefore(i,c[u])}}catch(e){l=!0,t=e}finally{try{r||null==n.return||n.return()}finally{if(l)throw t}}
+//# sourceMappingURL=index.c9e12236.js.map
---
+++ index.f75de5e1.js
@@ -0,0 +1,11 @@
+"use strict";
+const table = document.querySelector("table");
+const tableRows = table.rows;
+for (const row of tableRows){
+    const cells = row.cells;
+    const clonedCell = cells[1].cloneNode(true);
+    const lastCellIndex = cells.length - 1;
+    row.insertBefore(clonedCell, cells[lastCellIndex]);
+}
+
+//# sourceMappingURL=index.f75de5e1.js.map
